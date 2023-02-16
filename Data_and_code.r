a43361ac0329652b95b03e07a91151a79620bade
nicknick85 <39887415+nicknick85@users.noreply.github.com>
2023-02-16 23:22:28 +0300

    Update Data_and_code.r

--- Data_and_code.r
+++ Data_and_code.r
@@ -17,14 +17,15 @@ expit <- function(x)
 
 #############################################################################
 
-y  <- logit(incd[5:17]/100000)
+y1 <- logit(incd[5:17]/100000)
+y2 <- logit(mbt[5:17]/100000)
 x1 <- logit(mbt[4:16]/100000)
 x2 <- logit(scr[4:16]/100)
 x3 <- logit(scr[5:17]/100)
 
 #############################################################################
 
-dy <- y - logit(incd[4:16]/100000)
+dy <- y1 - logit(incd[4:16]/100000)
 
 png(file = "d_logit_incd.png", width=15000, height=5000, res=1200)
 boxplot(dy, outpch = 21, outbg="red", outcex=1.5, horizontal = TRUE)
@@ -39,4 +40,40 @@ grubbs.test(dy, type = 10)
 
 #############################################################################
 
-mdl <- lm(y ~ x1 + x2 + x3)
+mdl1 <- lm(y1 ~ x1 + x2 + x3)
+mdl2 <- lm(y2 ~ x1 + x2 + x3)
+
+#############################################################################
+
+scen <- function(mdl1, mdl2, s0, m0, s.new)
+{
+	m.prev <- m0;
+	s.prev <- s0;
+	m.new <- c();
+	i.new <- c();
+		
+	for (s in s.new)
+	{
+		m <- predict(mdl2, data.frame(x1 = m.prev, x2 = s.prev, x3 = s));
+		i <- predict(mdl1, data.frame(x1 = m.prev, x2 = s.prev, x3 = s));
+		m.new <- c(m.new, m);
+		i.new <- c(i.new, i)
+
+		m.prev <- m;
+		s.prev <- s;
+	}
+
+	list(m.new = m.new, i.new = i.new)
+}
+
+s.new1 <- logit(c(47.37, 58.95, 60.00, 62.11, 63.16, 90.00) / 100)
+s.new2 <- logit(c(82.11, 85.26, 86.32, 88.42, 89.47, 90.00) / 100)
+
+sc1 <- scen(mdl1, mdl2, x3[13], y2[13], s.new1);
+sc2 <- scen(mdl1, mdl2, x3[13], y2[13], s.new2);
+
+expit(sc1$m.new) * 100000
+expit(sc2$m.new) * 100000
+
+expit(sc1$i.new) * 100000
+expit(sc2$i.new) * 100000
